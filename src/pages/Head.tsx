fb2b019202bb8f6a44d716f8f733260b144830ae
0xfrian <0xfrian@gmail.com>
2023-01-02 02:04:40 -0800

    Removed <meta> thumbnail image tag

--- src/pages/Head.tsx
+++ src/pages/Head.tsx
@@ -3,7 +3,11 @@ import NextHead from "next/head"
 export default function Head() {
   return (
     <NextHead>
+      { /* Title + Favicon */ }
       <title>thecoreloop</title>
+      <link rel="icon" type="image/x-icon" href="/thecoreloop-favicon.png" />
+
+      { /* Metadata */ }
       <meta charSet="UTF-8" />
       <meta property="title" content="thecoreloop" />
       <meta property="og:title" content="thecoreloop" />
@@ -11,10 +15,9 @@ export default function Head() {
       <meta name="og:description" content="Gaming aggregator at the intersection of Web2 and Web3, curated by @0xkapital_k" />
       <meta property="type" content="blog" />
       <meta property="og:type" content="blog" />
-      <meta property="image" content="https://i.imgur.com/OXjCetQ.png" />
-      <meta property="og:image" content="https://i.imgur.com/OXjCetQ.png" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" type="image/x-icon" href="/thecoreloop-favicon.png" />
+
+      { /* Google Fonts */ }
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,800;1,400&family=Urbanist&display=swap" rel="stylesheet" />
