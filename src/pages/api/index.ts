1a888d289f3bdb1f32bca948b5b55d86077c9661
0xfrian <0xfrian@gmail.com>
2023-01-12 05:35:28 -0800

    Added error-handling

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -11,12 +11,26 @@ import { MongoClient } from "mongodb";
 export default async function handler (request: NextApiRequest, response: NextApiResponse) {
   console.log("Request received at api/");
 
-  // Create MongoDB Client
-  const uri: string = process.env.MONGODB_URI!;
-  const client: MongoClient = await createMongoDBClient(uri);
+  // Initialize <LAG> object
+  let lag: LAG = {
+    heading: "",
+    subheading: "",
+    message_id: -1,
+    number: -1,
+    date: "",
+    content: [],
+  };
 
-  // Read LAG #130
-  const lag: LAG = await readLAG(client, 130);
+  try {
+    // Create MongoDB Client
+    const uri: string = process.env.MONGODB_URI!;
+    const client: MongoClient = await createMongoDBClient(uri);
+
+    // Read LAG #130
+    lag = await readLAG(client, 130);
+  } catch (error) {
+    console.log(error);
+  }
 
   // Respond with JSON object
   response.json(lag);
