ff15e9c24e1ca86ac42485341642f4ab6fcf664f
0xfrian <0xfrian@gmail.com>
2023-02-04 16:57:30 -0800

    Fixed bug where article properties would be modified globally (unintentionally); Now uses a copy of the passed-in article argument

--- src/components/DailyLAG/Card.tsx
+++ src/components/DailyLAG/Card.tsx
@@ -1,11 +1,13 @@
+import { Article } from "../../types";
 import Card_Portrait from "./Card_Portrait";
 import Card_Landscape from "./Card_Landscape";
 
 export default function Card({ orientation, article }: any) {
+  const article_copy: Article = {...article};
   if (orientation == "landscape") {
-    return <Card_Landscape article={article} />; 
+    return <Card_Landscape article={article_copy} />; 
   } else {
-    return <Card_Portrait article={article} />; 
+    return <Card_Portrait article={article_copy} />; 
   }
 }
 
