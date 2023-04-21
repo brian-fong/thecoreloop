4bae3dc73ca8301586513feef292ca5d3fabda53
0xfrian <0xfrian@gmail.com>
2023-04-21 12:21:43 -0700

    Created 1st implementation of server-side authentication using AuthNext.js

---
+++ src/pages/api/login.tsx
@@ -0,0 +1,17 @@
+// Next.js
+import { NextApiRequest, NextApiResponse } from "next";
+
+// NextAuth.js
+import { auth_options } from "./auth/[...nextauth]";
+import { getServerSession } from "next-auth/next"
+
+export default async function login(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const session = await getServerSession(req, res, auth_options);
+  console.log("Request received: ", req.url);
+  console.log("Session: ", session);
+  res.end();
+}
+
