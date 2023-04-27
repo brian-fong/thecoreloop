743fcc32627479c60e33f1a462751de7f1c67c7c
DongChoi <dongandrewchoi@gmail.com>
2023-04-26 18:52:45 -0700

    removing seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 // import { genres } from "./data/genres";
 // const prisma = new PrismaClient();
 // import DATA from "./data/projects";
