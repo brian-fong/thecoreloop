dd99d7ea2a614c4eedbd36d87db4e9b2ed3c17a4
DongChoi <dongandrewchoi@gmail.com>
2023-05-01 17:20:10 -0700

    Adds game context to be shared throughout the app

--- src/context/context.js
+++ src/context/context.js
@@ -4,7 +4,7 @@ export const UserData = createContext(null);
 
 function Context({ children }) {
   const [currUser, setCurrUser] = useState();
-
+  const [gameData, setGameData] = useState();
   return (
     <UserData.Provider value={{ currUser, setCurrUser }}>
       {children}
