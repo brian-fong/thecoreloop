cbe88a61ffe794e44eba4fcbc0eed7cfebfd42b1
0xfrian <0xfrian@gmail.com>
2023-08-20 17:34:08 -0700

    ✨ feat: thecoreloop.gg/onchain subdomain redirects to notion

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/onchain",
+        destination: "https://thecoreloop.notion.site/On-Chain-Space-Station-9dd9e3579d984cd8bb0bc2e4a5b7f8f2?pvs=4",
+        permanent: true,
+      },
+    ];
+  },
+};
