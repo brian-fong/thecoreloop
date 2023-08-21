6ff5f06efe0b18b5a85875e6b269ca266f112d05
0xfrian <0xfrian@gmail.com>
2023-08-20 17:40:25 -0700

    ✨ feat: thecoreloop.gg/telegram redirects to telegram channel

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ module.exports = {
         destination: "https://thecoreloop.notion.site/On-Chain-Space-Station-9dd9e3579d984cd8bb0bc2e4a5b7f8f2?pvs=4",
         permanent: true,
       },
+      {
+        source: "/telegram",
+        destination: "https://t.me/thecoreloop",
+        permanent: true,
+      }
     ];
   },
 };
